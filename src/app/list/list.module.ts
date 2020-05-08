3461ee19ae93d7e1b2aea28c62ae8ce4691c76fb
Andrei Lukashenka <lukashenkoandrey@gmail.com>
2020-05-08 23:17:23 +0300

    Move export router module to routing

--- src/app/list/list.module.ts
+++ src/app/list/list.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
 
 import { ListRoutingModule } from './list-routing.module';
 
@@ -8,7 +7,7 @@ import { ListsPageComponent } from './pages/lists-page/lists-page.component';
 import { ListPageComponent } from './pages/list-page/list-page.component';
 
 @NgModule({
-  imports: [CommonModule, ListRoutingModule, RouterModule],
+  imports: [CommonModule, ListRoutingModule],
   declarations: [ListsPageComponent, ListPageComponent],
 })
 export class ListModule {}
