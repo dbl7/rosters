ef3ea472b582c459f2e40ae364a99775f379d1b0
Andrei Lukashenka <lukashenkoandrey@gmail.com>
2020-05-26 16:41:22 +0300

    Run tests in chrome headless for travis

--- karma.conf.js
+++ karma.conf.js
@@ -28,5 +28,11 @@ module.exports = function (config) {
     browsers: ['Chrome'],
     singleRun: false,
     restartOnFileChange: true,
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox'],
+      },
+    },
   });
 };
