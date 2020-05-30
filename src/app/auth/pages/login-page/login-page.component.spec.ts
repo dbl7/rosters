d74c93c1a985512b70ae69d95bf84932129f7b0d
Andrei Lukashenka <lukashenkoandrey@gmail.com>
2020-05-30 17:59:47 +0300

    fix(tests): get rid of firebase dependencies in components tests

--- src/app/auth/pages/login-page/login-page.component.spec.ts
+++ src/app/auth/pages/login-page/login-page.component.spec.ts
@@ -1,13 +1,16 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { AngularFireModule } from '@angular/fire';
 import { RouterTestingModule } from '@angular/router/testing';
-
-import { environment } from 'src/environments/environment';
-import { AppMaterialModule } from '@shared/material/app-material.module';
-
-import { LoginPageComponent } from './login-page.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+import { LoginPageComponent } from './login-page.component';
+import { AppMaterialModule } from '@shared/material/app-material.module';
+import { AuthService } from '@app/auth/auth.service';
+
+const AuthServiceStub = {
+  googleSignIn: () => {},
+  emailLogin: () => {},
+};
 
 describe('LoginPageComponent', () => {
   let component: LoginPageComponent;
@@ -16,13 +19,8 @@ describe('LoginPageComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [LoginPageComponent],
-      imports: [
-        AppMaterialModule,
-        AngularFireModule.initializeApp(environment.firebase),
-        RouterTestingModule,
-        ReactiveFormsModule,
-        NoopAnimationsModule,
-      ],
+      imports: [AppMaterialModule, RouterTestingModule, ReactiveFormsModule, NoopAnimationsModule],
+      providers: [{ provide: AuthService, useValue: AuthServiceStub }],
     }).compileComponents();
   }));
 
