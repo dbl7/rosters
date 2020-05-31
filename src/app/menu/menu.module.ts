839f437a06b6e202af0d00dada8e5f20e748c087
Andrei Lukashenka <lukashenkoandrey@gmail.com>
2020-05-31 11:53:43 +0300

    feat(menu): use the real user's lists

--- src/app/menu/menu.module.ts
+++ src/app/menu/menu.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
 
 import { SharedModule } from '@shared/shared.module';
 import { ListSwitchComponent } from './components/list-switch/list-switch.component';
@@ -8,7 +7,7 @@ import { MenuComponent } from './menu.component';
 
 @NgModule({
   declarations: [MenuComponent, ListSwitchComponent],
-  imports: [CommonModule, RouterModule, SharedModule],
+  imports: [CommonModule, SharedModule],
   exports: [MenuComponent],
 })
 export class MenuModule {}
